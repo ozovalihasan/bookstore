3e33427ba46a71f58239fbb472ca7c3e9b4788f5
Uduak Essien <acushla4real@gmail.com>
2020-11-25 15:59:45 +0100

    refactor BookForm to implement handleChange method

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -16,28 +16,34 @@ function BooksForm() {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('Action');
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (title && category) {
+      dispatch(addBook(title, category));
+      setTitle('');
+      setCategory('Action');
+      e.target.reset();
+    }
+  };
+
+  const handleChange = e => {
+    if (e.target.name === 'title') setTitle(e.target.value);
+    if (e.target.name === 'category') setCategory(e.target.value);
+  };
+
   return (
     <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (title && category) {
-            dispatch(addBook(title, category));
-            setTitle('');
-            setCategory('Action');
-            e.target.reset();
-          }
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <input
-          onChange={title => setTitle(title.target.value)}
+          name="title"
+          onChange={handleChange}
           type="text"
           placeholder="Title"
           required
         />
 
         <select
-          onChange={category => setCategory(category.target.value)}
+          onChange={handleChange}
           name="category"
           required
         >
