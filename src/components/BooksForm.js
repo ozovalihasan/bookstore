6850863de5e69fe2ff73be499fb85ef2a52ec343
Uduak Essien <acushla4real@gmail.com>
2020-11-25 15:15:16 +0100

    update BookForm component to require title and category

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -14,25 +14,32 @@ const CATEGORIES = [
 function BooksForm() {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState('Action');
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault();
-          dispatch(addBook(title, category));
+          if (title && category) {
+            dispatch(addBook(title, category));
+            setTitle('');
+            setCategory('');
+            e.target.reset();
+          }
         }}
       >
         <input
           onChange={title => setTitle(title.target.value)}
           type="text"
           placeholder="Title"
+          required
         />
 
         <select
           onChange={category => setCategory(category.target.value)}
           name="category"
+          required
         >
           <option disabled value>
             Select a category
