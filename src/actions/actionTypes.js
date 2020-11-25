f2bb20c15e6ed41357c4cc47d41b6c23d9a8773f
Uduak Essien <acushla4real@gmail.com>
2020-11-25 11:10:15 +0100

    add book types and actions

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const ADD_BOOK = 'ADD_BOOK';
---
+++ src/actions/bookActions.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import { ADD_BOOK } from './actionTypes';
+
+export const addBook = () => ({
+  type: ADD_BOOK,
+});
