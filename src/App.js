86215a38160b4374bcbb3ce30ae9599775a1020c
Uduak Essien <acushla4real@gmail.com>
2020-11-25 11:47:26 +0100

    Add connection from redux-store to our react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import BooksList from './components/BooksList';
 import BooksForm from './components/BooksForm';
+import store from './store';
 
 function App() {
   return (
-    <div>
-      <BooksList />
-      <BooksForm />
-    </div>
+    <Provider store={store}>
+      <div>
+        <BooksList />
+        <BooksForm />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function BooksList() {
+  const allBooks = useSelector(state => state.book);
   return (
     <div>
       <h2>Books List</h2>
+      <table>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+        {allBooks.map(book => (
+          <tr key={book.id}>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.category}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
