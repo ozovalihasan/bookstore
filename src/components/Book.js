8cf0542eb646c70e9162ba240c93426dfdacee54
Uduak Essien <acushla4real@gmail.com>
2020-11-25 15:42:42 +0100

    refactor onClick method to handleRemoveBook function

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,12 +5,13 @@ import { removeBook } from '../actions/index';
 
 function Book({ id, title, category }) {
   const dispatch = useDispatch();
+  const handleRemoveBook = () => dispatch(removeBook(id));
   return (
     <tr key={id}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
-      <td><button type="button" onClick={() => dispatch(removeBook(id))}>Remove Book</button></td>
+      <td><button type="button" onClick={handleRemoveBook}>Remove Book</button></td>
     </tr>
   );
 }
