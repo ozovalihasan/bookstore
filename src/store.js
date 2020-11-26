c1d4be343227eb0600c27fcecc3eaa6b0dc91bf8
Uduak Essien <acushla4real@gmail.com>
2020-11-26 10:40:55 +0100

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+  /* eslint-disable no-underscore-dangle */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
